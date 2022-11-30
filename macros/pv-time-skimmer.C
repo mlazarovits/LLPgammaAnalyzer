d28f8ad24a7316c0a54966310eda026761b763b7
mlazarovits <mrlazarovits@gmail.com>
2022-11-30 16:43:20 -0600

    initialized new skimmer for pv time analysis

---
+++ macros/pv-time-skimmer.C
@@ -0,0 +1,19 @@
+#include <iostream>
+
+
+
+using std::cout;
+using std::endl;
+
+
+
+int main(int argc, char* argv[]){
+	cout << "Look at all these chickens!" << endl;	
+
+	auto indir = "LLPGamma/llpga_GMSB_AOD_v53/";
+	auto infilename = "llpgana_mc_AODSIM_GMSB_AOD_v53_Full.txt";
+	auto outfilename = "llpgana_mc_AODSIM_GMSB_AOD_v53_pv-time-skim.root";
+	
+	
+
+}
