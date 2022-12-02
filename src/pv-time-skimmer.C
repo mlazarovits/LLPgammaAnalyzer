ee2416145a11f7f0ce6d5faa3f7aafef89b73456
mlazarovits <mrlazarovits@gmail.com>
2022-12-02 16:28:32 -0600

    added ReducedBase class (formerly llpgana_hist_base_v9)

--- src/pv-time-skimmer.C
+++ src/pv-time-skimmer.C
@@ -1,20 +1,52 @@
 #include <iostream>
-#include "llpgana_hist_base_v9.hh"
+#include <fstream>
+#include "ReducedBase.hh"
 
 
 using std::cout;
 using std::endl;
-
+using std::string;
 
 
 int main(int argc, char* argv[]){
 	cout << "Look at all these chickens!" << endl;	
 
-	auto indir = "LLPGamma/llpga_GMSB_AOD_v53/";
-	auto infilename = "llpgana_mc_AODSIM_GMSB_AOD_v53_Full.txt";
-	auto outfilename = "llpgana_mc_AODSIM_GMSB_AOD_v53_pv-time-skim.root";
+	string indir = "LLPGamma/llpga_GMSB_AOD_v55/";
+	string infilename = "llpgana_mc_AODSIM_GMSB_AOD_v55_Full.txt";
+	string outfilename = "llpgana_mc_AODSIM_GMSB_AOD_v55_pv-time-skim.root";
+
+	string eosdir = "root://cmseos.fnal.gov//store/user/lpcsusylep/jaking/";
 	
-	const std::string treename("tree/llpgana");
-		
+	string treeName = "tree/llpgana";
+	string infilelist = "llpgana_mc_AODSIM_GMSB_AOD_v55_Full.txt";	
+	std::ifstream infile(infilelist);
+	
+	TChain* ch = new TChain(treeName.c_str());
+	//get trees from files
+	string str;	
+	int pct = 10;
+	int cnt = 1;
+	cout << "adding every 1 out of " << pct << " files from " << eosdir+indir+infilelist << endl;
+	while(std::getline(infile, str)){
+		string tfilename = eosdir + indir + str;
+		if(cnt%pct == 0){
+			std::cout << "--  adding file: " << tfilename << std::endl;
+			ch->Add(tfilename.c_str());
+		}
+		cnt++;
+	}
+
+	//initialize chain with make class
+	ReducedBase* base = new ReducedBase(ch);	
+	//loop through events
+	int nEntries = ch->GetEntries();
+	cout << nEntries << endl;
+	for(int i = 0; i < nEntries; i++){
+		if (i % 10000 == 0) cout << "Processed " << i << " of " << nEntries << endl;
+			
+	}
+
+
+
 
 }
