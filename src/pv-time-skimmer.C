46a91c6e679dbf9da56eca0681c189e65d337f1b
mlazarovits <mrlazarovits@gmail.com>
2022-12-02 17:14:35 -0600

    update

--- src/pv-time-skimmer.C
+++ src/pv-time-skimmer.C
@@ -9,16 +9,15 @@ using std::string;
 
 
 int main(int argc, char* argv[]){
-	cout << "Look at all these chickens!" << endl;	
+	cout << "Look at all those chickens!" << endl;	
 
 	string indir = "LLPGamma/llpga_GMSB_AOD_v55/";
-	string infilename = "llpgana_mc_AODSIM_GMSB_AOD_v55_Full.txt";
-	string outfilename = "llpgana_mc_AODSIM_GMSB_AOD_v55_pv-time-skim.root";
+	string outfilename = "root/llpgana_mc_AODSIM_GMSB_AOD_v55_pv-time-skim.root";
 
 	string eosdir = "root://cmseos.fnal.gov//store/user/lpcsusylep/jaking/";
 	
-	string treeName = "tree/llpgana";
-	string infilelist = "llpgana_mc_AODSIM_GMSB_AOD_v55_Full.txt";	
+	string treeName = "tree/llpgtree";
+	string infilelist = "fileLists/llpgana_mc_AODSIM_GMSB_AOD_v55_Full.txt";	
 	std::ifstream infile(infilelist);
 	
 	TChain* ch = new TChain(treeName.c_str());
@@ -26,11 +25,11 @@ int main(int argc, char* argv[]){
 	string str;	
 	int pct = 10;
 	int cnt = 1;
-	cout << "adding every 1 out of " << pct << " files from " << eosdir+indir+infilelist << endl;
+	cout << "adding every 1 out of " << pct << " files from " << infilelist << endl;
 	while(std::getline(infile, str)){
 		string tfilename = eosdir + indir + str;
 		if(cnt%pct == 0){
-			std::cout << "--  adding file: " << tfilename << std::endl;
+			if(cnt%(pct*100) == 0) std::cout << "Processed " << cnt << " files" << std::endl;
 			ch->Add(tfilename.c_str());
 		}
 		cnt++;
@@ -40,11 +39,18 @@ int main(int argc, char* argv[]){
 	ReducedBase* base = new ReducedBase(ch);	
 	//loop through events
 	int nEntries = ch->GetEntries();
-	cout << nEntries << endl;
 	for(int i = 0; i < nEntries; i++){
-		if (i % 10000 == 0) cout << "Processed " << i << " of " << nEntries << endl;
+		if (i % 100000 == 0) cout << "Processed " << i << " of " << nEntries << endl;
 			
 	}
+
+
+
+	TFile* outFile = new TFile(outfilename.c_str(),"RECREATE");
+	outFile->cd();
+	//write shit here to file
+	cout << "Writing out to " << outfilename << endl;
+	outFile->Close();
 
 
 
